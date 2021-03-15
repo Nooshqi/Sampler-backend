4e4c6c2a840b0dfbeca34c5e4aed476be35cca5b
BRYAN KINUTHIA <bkinuthi@uwaterloo.ca>
2021-03-14 21:26:37 -0400

    testing connection issues

--- _helpers/db.js
+++ _helpers/db.js
@@ -10,7 +10,7 @@ async function initialize() {
     // create db if it doesn't already exist
     const { host, port, user, password, database } = config.database;
     // const connection = await mysql.createConnection({ host, port, user, password });
-    const connection = await mysql.createConnection({ host, user, password, database });
+    const connection = await mysql.createConnection({ host, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
